a65c5a19bcf33b37facef0fb14904ac611586aad
joelgrod <joelgrod@gmail.com>
2021-04-05 12:34:10 +0100

    markers and properties pages

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, Input, ViewChild, ElementRef } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-map',
   templateUrl: './mini-map.component.html',
   styles: [
+    `
+    .map-container {
+      width: 100%;
+      height: 10rem;
+      margin: 0px;
+    }
+    `
   ]
 })
-export class MiniMapComponent implements OnInit {
+export class MiniMapComponent implements AfterViewInit {
+  
+  @Input() lngLat: [number, number] = [0, 0];
+  @ViewChild('map') map_container!: ElementRef;
 
-  constructor() { }
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: this.map_container.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: this.lngLat,
+      zoom: 15,
+      interactive: false
+    });
 
-  ngOnInit(): void {
+    new mapboxgl.Marker()
+      .setLngLat(this.lngLat)
+      .addTo(map);
   }
+
+  
 
 }
--- src/app/maps/pages/markers/markers.component.ts
+++ src/app/maps/pages/markers/markers.component.ts
@@ -6,6 +6,11 @@ interface BaseMarker {
   color: string;
 }
 
+interface LocalBaseMarker {
+  center: [number, number];
+  color: string;
+}
+
 @Component({
   selector: 'app-markers',
   templateUrl: './markers.component.html',
@@ -18,10 +23,11 @@ interface BaseMarker {
 
     .add-button {
       cursor: pointer;
+      text-align: center;
       position: fixed;
       top: 1rem;
       right: 1rem;
-      width: 5rem;
+      width: 6rem;
       z-index: 1;
     }
     `
@@ -48,8 +54,10 @@ export class MarkersComponent implements AfterViewInit, OnDestroy {
       container: this.map_container.nativeElement,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: this.center,
-      zoom: this.zoom_value
+      zoom: 0
     });
+
+    this.read_local_storage();
 
     // Custom marker example:
 
@@ -87,22 +95,69 @@ export class MarkersComponent implements AfterViewInit, OnDestroy {
       marker: newMarker,
       color: color
     });
+
+    newMarker.on('dragend', () => {
+      this.save_markers_local_storage();
+    });
+
+    this.save_markers_local_storage();
   }
 
   go_marker(marker: mapboxgl.Marker): void {
     this.map.flyTo({
-      center: marker.getLngLat()
+      center: marker.getLngLat(),
+      zoom: this.zoom_value
     });
+  }
+
+  // Dbl click for delete
+  delete_marker(index: any) {
+    this.markers[index].marker?.remove();   // Remove object
+    this.markers.splice(index, 1);          // Remove index
+    this.save_markers_local_storage();      // Update local storage
   }
 
   // Optimal: save in backend but for this 
   // example we put markers on local storage
   save_markers_local_storage(): void {
+    const local_markers: LocalBaseMarker[] = [];
 
+    this.markers.forEach( marker => {
+      const color = marker.color;
+      const {lng, lat} = marker.marker.getLngLat();
+
+      local_markers.push({
+        center: [lng, lat],
+        color: color
+      })
+    });
+
+    localStorage.setItem('markers', JSON.stringify(local_markers));
   }
 
   read_local_storage(): void {
-    
+    if(!localStorage.getItem('markers')) return;
+
+    const local_markers: LocalBaseMarker[] = JSON.parse(localStorage.getItem('markers')!);
+
+    local_markers.forEach( marker => {
+      const newMarker = new mapboxgl.Marker({
+        draggable: true,
+        color: marker.color
+      })
+        .setLngLat(marker.center)
+        .addTo(this.map);
+      
+      
+      this.markers.push({
+        marker: newMarker,
+        color: marker.color
+      });
+
+      newMarker.on('dragend', () => {
+        this.save_markers_local_storage();
+      });
+    });
   }
 
 }
